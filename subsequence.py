c4ef755ecc82c5261f2928ae3bd767638513be84
baskarrajaram <143525714+baskarrajaram@users.noreply.github.com>
2023-10-01 14:17:54 +0530

    Create subsequence.py

---
+++ subsequence.py
@@ -0,0 +1,12 @@
+def isSubsequence(s, t):
+    i, j = 0, 0
+     while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+  return i == len(s)
+s = "abc"
+t = "ahbgdc"
+result = isSubsequence(s, t)
+print(result)  
+
