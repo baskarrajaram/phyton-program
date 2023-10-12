fd1728aba1b46b27115585434059270cb7459310
baskarrajaram <143525714+baskarrajaram@users.noreply.github.com>
2023-10-12 21:32:37 +0530

    Create int to rom.py

---
+++ int to rom.py
@@ -0,0 +1,17 @@
+def int_to_roman(num):
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    syms = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    roman_numeral = ""
+    
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            roman_numeral += syms[i]
+            num -= val[i]
+        i += 1
+        
+    return roman_numeral
+
+integer = 3549
+roman = int_to_roman(integer)
+print(f"{integer} in Roman numerals is {roman}")
